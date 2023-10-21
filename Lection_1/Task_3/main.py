a8d6120dcbe68b9aead64bdd8610e44657120864
Sans91 <sansomailjokes@gmail.com>
2023-10-21 17:34:33 +0300

    Modified L_1/T_3. Organised repeating code

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -1,4 +1,5 @@
+temperature = float(input('Enter the temperature: '))
 if input("To which temperature metric you want to convert?(F/C): ") == "C":
-    print(f"{(float(input('Enter the temperature: '))-32) / 1.8} C")
+    print(f"{(temperature-32) / 1.8} C")
 else:
-    print(f"{float(input('Enter the temperature: '))*1.8 + 32} F")
+    print(f"{temperature*1.8 + 32} F")
